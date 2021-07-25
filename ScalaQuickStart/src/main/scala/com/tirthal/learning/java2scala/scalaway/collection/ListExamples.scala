8dfe812a47ebd0abb50d143beeb8e32f99f9f539
Tirthal Patel <patel.tirthal@gmail.com>
2021-07-25 16:12:02 +0530

    Minor changes in ListExamples code

--- ScalaQuickStart/src/main/scala/com/tirthal/learning/java2scala/scalaway/collection/ListExamples.scala
+++ ScalaQuickStart/src/main/scala/com/tirthal/learning/java2scala/scalaway/collection/ListExamples.scala
@@ -9,19 +9,28 @@ object ListExamples {
 
   def main(args: Array[String]) {
 
-    var list = List(1, 2, 3) // a var - mutable reference of a list
+    var list = List(1, 2, 3) // a var - mutable reference of a scala.collection.immutable.List
+    println(list.getClass)
     println(list.toString)
     list = list :+ 4 // :+ returns a new immutable List instance, with additional element appended
     println(list.toString)
 
-    // A ListBuffer is like an array buffer except that it uses a linked list internally instead of an array
+    // A ListBuffer (mutable) is like an array buffer except that it uses a linked list internally instead of an array
     // If you plan to convert the buffer to a list once it is built up, use a list buffer instead of an array buffer
-    val listBuffer = ListBuffer(1, 2, 3) // a val - immutable reference of a listBuffer
+    val listBuffer = ListBuffer(1, 2, 3) // a val - immutable reference of a scala.collection.mutable.ListBuffer
+    println(listBuffer.getClass)
     println(listBuffer.toString)
     listBuffer.addOne(4)  // content can be changed by adding element to existing list instance
     println(listBuffer.toString)
     listBuffer += 5;      // content can be changed by adding element to existing list instance
     println(listBuffer.toString)
     println(listBuffer.toList) // convert the buffer to a list
+
+    // Head = First element of list & Tail = All elements of list excluding the First one
+    println(listBuffer.head + " --- " + listBuffer.last + " --- " + listBuffer.tail)
+
+    // How to iterate list using "Generator Based For Loop", i.e. For(element <- generator)?
+    for(number <- listBuffer)
+      print(s"$number ")
   }
 }
